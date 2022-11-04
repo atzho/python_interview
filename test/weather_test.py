7fc813ea888573c649a825c76c2beba7ce340eed
Andrew Zhong <azhong2002@outlook.com>
2022-11-04 00:31:37 -0400

    Fix reads and add docs

--- test/weather_test.py
+++ test/weather_test.py
@@ -2,6 +2,9 @@ from interview import weather
 import io
 import pandas as pd
 
+'''
+Checks if the number of unique date/station pairs is the same as the original
+'''
 def length_check(reader, df):
     unique = set()
     for _, row in pd.read_csv(reader).iterrows():
@@ -9,6 +12,9 @@ def length_check(reader, df):
         unique.add((row['Station Name'], timestamp[0]))
     return len(df.index)==len(unique)
 
+'''
+Checks if the min and max are the min and max for that row
+'''
 def sanity_check(df):
     for _, row in df.iterrows():
         temps = [row['Max Temp'], row['Min Temp'], row['First Temp'], row['Last Temp']]
@@ -18,10 +24,17 @@ def sanity_check(df):
             return False
     return True
 
+'''
+Run tests. We call all tests together so that we only have to run
+process_csv one time.
+'''
 def test_suite():
     reader = "data/chicago_beach_weather.csv"
     writer = io.StringIO()
     weather.process_csv(reader, writer)
+
+    writer.seek(0)
     df = pd.read_csv(writer)
+
     assert length_check(reader, df)
     assert sanity_check(df)

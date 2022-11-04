9fca31fbddd7b3f2c20cae08b950d7de1ab5dc8a
Andrew Zhong <azhong2002@outlook.com>
2022-11-03 23:47:09 -0400

    Add length and sanity tests

--- test/weather_test.py
+++ test/weather_test.py
@@ -1,9 +1,27 @@
 from interview import weather
 import io
+import pandas as pd
 
+def length_check(writer, df):
+    unique = set()
+    for _, row in pd.read_csv(writer).iterrows():
+        timestamp = row['Measurement Timestamp Label'].split()
+        unique.add((row['Station Name'], timestamp[0]))
+    return len(df.index)==len(unique)
 
-def test_replace_me():
-    reader = io.StringIO("Line One\nLine Two\n")
+def sanity_check(df):
+    for _, row in df.iterrows():
+        temps = [row['Max Temp'], row['Min Temp'], row['First Temp'], row['Last Temp']]
+        if row['Max Temp'] != max(temps):
+            return False
+        if row['Min Temp'] != min(temps):
+            return False
+    return True
+
+def test_suite():
+    reader = "data/chicago_beach_weather.csv"
     writer = io.StringIO()
     weather.process_csv(reader, writer)
-    assert writer.getvalue() == "Saw 2 lines\n"
+    df = pd.read_csv(writer)
+    assert length_check(writer, df)
+    assert sanity_check(df)
